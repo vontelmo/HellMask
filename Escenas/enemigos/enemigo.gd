455e2a91793a39317fc4035cf0ca4fce72f3ca4a
vontelmo <115665511+vontelmo@users.noreply.github.com>
2026-01-29 16:16:53 -0300

    Rooms & enemigos

---
+++ Escenas/enemigos/enemigo.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+var player: CharacterBody2D
+var speed = 200
+
+func _ready():
+	player = get_tree().get_first_node_in_group("player")
+	
+func _physics_process(delta):
+	if not player:
+		return
+
+	var dir = (player.global_position - global_position).normalized()
+	velocity = dir * speed
+	move_and_slide()
