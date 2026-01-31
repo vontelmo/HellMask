ed00fd830d97356be1c9ae87e387beff3715e3cf
sbb <5mlbergman+12@gmail.com>
2026-01-31 03:52:29 -0300

    mato mostros

--- Escenas/armas/espada.gd
+++ Escenas/armas/espada.gd
@@ -1,11 +1,19 @@
 extends Arma
 
+@onready var hurtbox := $Hurtbox
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	cargas = 1
-	dmg = 600
+	dmg = 200
+	hurtbox.area_entered.connect(_on_hurtbox_area_entered)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _on_hurtbox_area_entered(area: Area2D):
+	if area.is_in_group("enemies"):
+		print(area)
+		area.get_parent()._take_damage(dmg)
